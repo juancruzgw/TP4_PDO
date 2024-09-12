51139eb600d301541adade7b0053f44d42fe6369
DiegoAvila1 <diegoavilaaa0@gmail.com>
2024-09-12 14:10:11 -0300

    arregle el css , faltaria el js

--- Vista/Action/accionCambioDuenio.php
+++ Vista/Action/accionCambioDuenio.php
@@ -1,20 +1,8 @@
-<?php include_once "../Estructura/Header.php";?>
 
+<?php include_once "../Estructura/Header.php";?>
 
 <div class="container cont-form">
 <?php
-/* 
--Crear   una   página   “CambioDuenio.php”   que   contenga   un   formulario   en   donde   se   solicite :
--numero de patente de un auto
--un numero de documento de una persona 
-- “accionCambioDuenio.php”   
--se   realice   cambio   del   dueño   del   auto   de   la   patente ingresada en el formulario.
--hay que cambiar de dueño con el numero de patente del auto y su numero de documento 
-
--Mostrar mensajes de error en caso de que el auto o la persona no se encuentren
-cargados. Utilizar css y validaciones javaScript cuando crea conveniente. Recordar usar la capa de control
-antes generada, no se puede acceder directamente a las clases del ORM
-*/
 
 include "../../utils/utils.php";
 include '../../Modelo/Persona.php';
@@ -34,16 +22,17 @@ if($datos['patente'] !== "null" && $datos['NroDni'] !== 'null'){
         $datos['accion'] = 'editar';
         $datos['marca'] = $auto->getMarca();
         $datos['modelo'] = $auto->getModelo();
+
         if($abmAuto->abm($datos)){
-            echo "Se cambio el dueño del auto";
+            echo "<div class = cambioDatos contenedorItems>Se cambio el dueño del auto</div>";
         }else{
-            echo "No se pudo cambiar el dueño del auto";
+            echo "<div class='NocambioDatos contenedorItems'>No se pudo cambiar el dueño del auto</div>";
         }
     } else {
-        echo "No se encontro el auto o la persona";
+        echo "<div class=personaNoEncontrada contenedorItems>No se encontro el auto o la persona</div>";
     }
 }else{
-    echo "no se ingresaron correctamente los datos";
+    echo "<div class=ingresoErroneoDatos contenedorItems>no se ingresaron correctamente los datos</div>";
 }
 
 
