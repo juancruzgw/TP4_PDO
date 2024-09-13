12f5f0f716e34f44bbf1c68739d3a88676d9a8c9
DiegoAvila1 <diegoavilaaa0@gmail.com>
2024-09-13 20:49:44 -0300

    Arreglo del layout del accionCamboDuenio.php

--- Vista/Action/accionCambioDuenio.php
+++ Vista/Action/accionCambioDuenio.php
@@ -8,30 +8,28 @@ $abmAuto = new AbmAuto();
 $datos = data_submitted();
 
 echo "<div class='container cont-form'>";
-
 try{
     if($datos['patente'] !== "null" && $datos['NroDni'] !== 'null'){
         if(!empty($auto = $abmAuto->obtenerDatos($datos)) && !empty($abmPersona->buscar($datos))){
-            
             $datos['accion'] = 'editar';
             $datos['marca'] = $auto['marca'];
             $datos['modelo'] = $auto["modelo"];
 
             if($abmAuto->abm($datos)){
-                echo "<div class ='modalDatosCorrecto' style='font-weight: bold; >Se cambio el dueño del auto</div>";
+                echo "<div class='modalDatosCorrecto' style='font-weight: bold;'>Se cambió el dueño del auto</div>";
             }else{
-                echo "<div class=' modalDatosIncorrectos'  style='font-weight: bold;> No se pudo cambiar el dueño del auto</div>";
+                echo "<div class=' modalDatosIncorrectos' style='font-weight: bold;'>No se pudo cambiar el dueño del auto</div>";
             }
 
         } else {
-            throw new exception("<div class =' modalDatosIncorrectos'  style='font-weight: bold;> No se encontro el auto o la persona </div>");
+            throw new Exception("<div class =' modalDatosIncorrectos'  style='font-weight: bold;'> No se encontro el auto o la persona </div>");
         }
     }else{
-        throw new exception("<div class =' modalDatosIncorrectos'  style='font-weight: bold;> No se ingresaron correctamente los datos</div>");
+        throw new Exception("<div class =' modalDatosIncorrectos'  style='font-weight: bold;'> No se ingresaron correctamente los datos</div>");
     }
 
 }catch(PDOException $ex){
-  echo "<div class ='modalDatosIncorrectos'".$ex->getMessage()."</div>";
+    echo "<div class='modalDatosIncorrectos' style='font-weight: bold;'>" . $ex->getMessage() . "</div>";
 }catch(Exception $ex){
     echo $ex->getMessage();
 }
